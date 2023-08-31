b12b5f98395d413c4f26a9ab06102a6d0627efce
arielcollatti <gabriel.rodriguesm0rg@outlook.com>
2023-08-31 20:10:01 -0300

    power

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const registerForm = document.getElementById("registerForm");
+  
+    registerForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      window.location.href = "index.html"; 
+    });
+  });
+   
