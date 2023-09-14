6dfeb9dfd980ae52de7d7eddaa96cf69bf711008
gnx9s <e107ti@etec.sp.gov.br>
2023-09-14 19:33:04 -0300

    atualização

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
     registerForm.addEventListener("submit", function(event) {
       event.preventDefault();
   
-      window.location.href = "index.html"; 
+      window.location.href = "confirmation.html"; 
     });
   });
