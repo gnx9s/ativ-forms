391ab324c6adbbd6922f6e0624b4ebcf1b843ace
gnx9s <e107ti@etec.sp.gov.br>
2023-09-14 20:32:36 -0300

    resolvendo JS

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ document.addEventListener("DOMContentLoaded", function() {
       window.location.href = "confirmation.html"; 
     });
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const modeToggle = document.querySelector('.modeToggle');
+    const body = document.body;
+    const container = document.getElementById('container');
+    const input = document.getElementById('password');
+    const input2 = document.getElementById('name');
+    const button = document.getElementById('button');
+
+    modeToggle.addEventListener('click', function() {
+      body.classList.toggle('darkMode');
+      container.classList.toggle('darkMode');
+
+      if(body.classList.contains('darkMode'))
+      {
+        body.style.background = '#142d4c';
+        body.style.backgroundColor = '#091727';
+        body.style.transition = '0.7s';
+        container.style.border = 'solid #081b31';
+        container.style.boxShadow = '4px 4px 0 0 #050d18';
+        container.style.transition = '0.7s';
+        input.style.borderColor = '#38598b';
+        input2.style.borderColor = '#38598b';
+        button.style.backgroundColor = '#113f67';
+      }
+
+      else 
+      {
+        body.style.backgroundColor = '#5c5470';
+        body.style.transition = '0.6s';
+        container.style.border = 'solid 1px';
+        container.style.boxShadow = '4px 4px 0 0 #24202c';
+        container.style.borderColor = '#352f44';
+        container.style.transition = '0.6s';
+        input.style.borderColor = '#352f44';
+        input2.style.borderColor = '#352f44';
+        button.style.backgroundColor = '#352f44';
+      }
+    });
+  });
