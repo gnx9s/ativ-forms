86c4db97db216864261ce15cc6614a0f0a4e929c
gnx9s <e107ti@etec.sp.gov.br>
2023-09-14 22:32:40 -0300

    resolver o JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const registerForm = document.getElementById("registerForm");
-  
+      
     registerForm.addEventListener("submit", function(event) {
       event.preventDefault();
   
@@ -12,25 +12,35 @@ document.addEventListener("DOMContentLoaded", function() {
     const modeToggle = document.querySelector('.modeToggle');
     const body = document.body;
     const container = document.getElementById('container');
-    const input = document.getElementById('password');
-    const input2 = document.getElementById('name');
+    // const input = document.getElementById('password');
+    // const input2 = document.getElementById('name');
     const button = document.getElementById('button');
 
+    const inputs = document.getElementsByTagName("input");
+    const buttons = document.getElementsByTagName('button');
+
     modeToggle.addEventListener('click', function() {
+
       body.classList.toggle('darkMode');
       container.classList.toggle('darkMode');
-
+      
       if(body.classList.contains('darkMode'))
       {
-        body.style.background = '#142d4c';
-        body.style.backgroundColor = '#091727';
+        body.style.background = '#606470';
+        body.style.backgroundColor = '#222831';
         body.style.transition = '0.7s';
-        container.style.border = 'solid #081b31';
+        container.style.border = 'solid #303841';
         container.style.boxShadow = '4px 4px 0 0 #050d18';
         container.style.transition = '0.7s';
-        input.style.borderColor = '#38598b';
-        input2.style.borderColor = '#38598b';
-        button.style.backgroundColor = '#113f67';
+        container.style.backgroundColor = '#363b4e';
+        document.querySelectorAll("input")
+          .forEach(input => input.style.borderColor = '#363b4e');        
+        // for(var i =0; i< inputs.length; i++) 
+        //   inputs[i].style.borderColor = '#38598b';        
+        // input.style.borderColor = '#38598b';
+        // input2.style.borderColor = '#38598b';
+        for(var i =0; i< buttons.length; i++) 
+          buttons[i].style.backgroundColor = '#363b4e';
       }
 
       else 
@@ -40,10 +50,17 @@ document.addEventListener("DOMContentLoaded", function() {
         container.style.border = 'solid 1px';
         container.style.boxShadow = '4px 4px 0 0 #24202c';
         container.style.borderColor = '#352f44';
+        container.style.backgroundColor = '#352f44';
         container.style.transition = '0.6s';
-        input.style.borderColor = '#352f44';
-        input2.style.borderColor = '#352f44';
-        button.style.backgroundColor = '#352f44';
+        for(var i =0; i< inputs.length; i++) 
+          inputs[i].style.borderColor = '#352f44';        
+        // input.style.borderColor = '#352f44';
+        // input2.style.borderColor = '#352f44';
+        for(var i =0; i< buttons.length; i++) 
+          buttons[i].style.backgroundColor = '#352f44';        
       }
     });
   });
+
+
+// #606470(mais claro), #393e46(escuro), #222831(escurao)
