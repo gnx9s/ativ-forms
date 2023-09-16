e2425d419a78ae08036fe2f3647e7b9ecfdceb5d
arielcollatti <gabriel.rodriguesm0rg@outlook.com>
2023-09-16 18:44:49 -0300

    cores alteradas

--- register.js
+++ register.js
@@ -83,46 +83,45 @@ document.addEventListener("DOMContentLoaded", function() {
 
     modeToggle.addEventListener('click', function() {
 
-      body.classList.toggle('darkMode');
-      container.classList.toggle('darkMode');
-      
-      if(body.classList.contains('darkMode'))
-      {
-        body.style.background = '#606470';
-        body.style.backgroundColor = '#222831';
-        body.style.transition = '0.7s';
-        container.style.border = 'solid #303841';
-        container.style.boxShadow = '4px 4px 0 0 #050d18';
-        container.style.transition = '0.7s';
-        container.style.backgroundColor = '#363b4e';
-        document.querySelectorAll("input")
-          .forEach(input => input.style.borderColor = '#363b4e');        
-        // for(var i =0; i< inputs.length; i++) 
-        //   inputs[i].style.borderColor = '#38598b';        
-        // input.style.borderColor = '#38598b';
-        // input2.style.borderColor = '#38598b';
-        for(var i =0; i< buttons.length; i++) 
-          buttons[i].style.backgroundColor = '#363b4e';
-      }
-
-      else 
-      {
-        body.style.backgroundColor = '#5c5470';
-        body.style.transition = '0.6s';
-        container.style.border = 'solid 1px';
-        container.style.boxShadow = '4px 4px 0 0 #24202c';
-        container.style.borderColor = '#352f44';
-        container.style.backgroundColor = '#352f44';
-        container.style.transition = '0.6s';
-        for(var i =0; i< inputs.length; i++) 
-          inputs[i].style.borderColor = '#352f44';        
-        // input.style.borderColor = '#352f44';
-        // input2.style.borderColor = '#352f44';
-        for(var i =0; i< buttons.length; i++) 
-          buttons[i].style.backgroundColor = '#352f44';        
-      }
+        body.classList.toggle('darkMode');
+        container.classList.toggle('darkMode');
+        
+        if(body.classList.contains('darkMode'))
+        {
+          body.style.background = '#141010';
+          body.style.backgroundColor = '#141010';
+          body.style.transition = '0.7s';
+          container.style.border = 'solid 1px #363434';
+          container.style.boxShadow = '4px 4px 0 0 #060608';
+          container.style.transition = '0.7s';
+          container.style.backgroundColor = '#222831';
+          document.querySelectorAll("input")
+            .forEach(input => input.style.borderColor = '#363434');        
+          // for(var i =0; i< inputs.length; i++) 
+          //   inputs[i].style.borderColor = '#38598b';        
+          // input.style.borderColor = '#38598b';
+          // input2.style.borderColor = '#38598b';
+          for(var i =0; i< buttons.length; i++) 
+            buttons[i].style.backgroundColor = '#222831';
+        }
+  
+        else 
+        {
+          body.style.backgroundColor = '#e3e3e3';
+          body.style.transition = '0.7s';
+          container.style.border = 'solid 1px';
+          container.style.boxShadow = '4px 4px 0 0 #393e46';
+          container.style.backgroundColor = '#393e46';
+          container.style.transition = '0.7s';
+          for(var i =0; i< inputs.length; i++) 
+            inputs[i].style.borderColor = '#352f44';        
+          // input.style.borderColor = '#352f44';
+          // input2.style.borderColor = '#352f44';
+          for(var i =0; i< buttons.length; i++) 
+            buttons[i].style.backgroundColor = '#393e46';      
+        }
+      });
     });
-  });
-
+  
 
 // #606470(mais claro), #393e46(escuro), #222831(escurao)
--- script.js
+++ script.js
@@ -16,15 +16,15 @@
       
       if(body.classList.contains('darkMode'))
       {
-        body.style.background = '#606470';
-        body.style.backgroundColor = '#363b4e';
+        body.style.background = '#141010';
+        body.style.backgroundColor = '#141010';
         body.style.transition = '0.7s';
-        container.style.border = 'solid #303841';
-        container.style.boxShadow = '4px 4px 0 0 #050d18';
+        container.style.border = 'solid 1px #363434';
+        container.style.boxShadow = '4px 4px 0 0 #060608';
         container.style.transition = '0.7s';
         container.style.backgroundColor = '#222831';
         document.querySelectorAll("input")
-          .forEach(input => input.style.borderColor = '#222831');        
+          .forEach(input => input.style.borderColor = '#363434');        
         // for(var i =0; i< inputs.length; i++) 
         //   inputs[i].style.borderColor = '#38598b';        
         // input.style.borderColor = '#38598b';
@@ -35,19 +35,18 @@
 
       else 
       {
-        body.style.backgroundColor = '#5c5470';
-        body.style.transition = '0.6s';
+        body.style.backgroundColor = '#e3e3e3';
+        body.style.transition = '0.7s';
         container.style.border = 'solid 1px';
-        container.style.boxShadow = '4px 4px 0 0 #24202c';
-        container.style.borderColor = '#352f44';
-        container.style.backgroundColor = '#352f44';
-        container.style.transition = '0.6s';
+        container.style.boxShadow = '4px 4px 0 0 #393e46';
+        container.style.backgroundColor = '#393e46';
+        container.style.transition = '0.7s';
         for(var i =0; i< inputs.length; i++) 
           inputs[i].style.borderColor = '#352f44';        
         // input.style.borderColor = '#352f44';
         // input2.style.borderColor = '#352f44';
         for(var i =0; i< buttons.length; i++) 
-          buttons[i].style.backgroundColor = '#352f44';      
+          buttons[i].style.backgroundColor = '#393e46';      
       }
     });
   });
