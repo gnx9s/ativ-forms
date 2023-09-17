4117a75b7c978f7a5e3eb76bdb51ebd66d662d3c
arielcollatti <gabriel.rodriguesm0rg@outlook.com>
2023-09-17 02:19:44 -0300

    alterando cores

--- script.js
+++ script.js
@@ -16,13 +16,12 @@
       
       if(body.classList.contains('darkMode'))
       {
-        body.style.background = '#141010';
-        body.style.backgroundColor = '#141010';
+        body.style.backgroundColor = '#161616';
         body.style.transition = '0.7s';
-        container.style.border = 'solid 1px #363434';
+        container.style.border = 'solid 1px black';
         container.style.boxShadow = '4px 4px 0 0 #060608';
         container.style.transition = '0.7s';
-        container.style.backgroundColor = '#222831';
+        container.style.backgroundColor = '#1F1F1F';
         document.querySelectorAll("input")
           .forEach(input => input.style.borderColor = '#363434');        
         // for(var i =0; i< inputs.length; i++) 
@@ -30,24 +29,24 @@
         // input.style.borderColor = '#38598b';
         // input2.style.borderColor = '#38598b';
         for(var i =0; i< buttons.length; i++) 
-          buttons[i].style.backgroundColor = '#222831';
+          buttons[i].style.backgroundColor = '#1F1F1F';
       }
 
       else 
-      {
-        body.style.backgroundColor = '#e3e3e3';
-        body.style.transition = '0.7s';
-        container.style.border = 'solid 1px';
-        container.style.boxShadow = '4px 4px 0 0 #393e46';
-        container.style.backgroundColor = '#393e46';
-        container.style.transition = '0.7s';
-        for(var i =0; i< inputs.length; i++) 
-          inputs[i].style.borderColor = '#352f44';        
-        // input.style.borderColor = '#352f44';
-        // input2.style.borderColor = '#352f44';
-        for(var i =0; i< buttons.length; i++) 
-          buttons[i].style.backgroundColor = '#393e46';      
-      }
+        {
+          body.style.backgroundColor = 'whitesmoke';
+          body.style.transition = '0.7s';
+          container.style.border = 'solid 1px #393E46';
+          container.style.boxShadow = '4px 4px 0 0 ';
+          container.style.backgroundColor = '393e46';
+          container.style.transition = '0.7s';
+          for(var i =0; i< inputs.length; i++) 
+            inputs[i].style.borderColor = '#352f44';        
+          // input.style.borderColor = '#352f44';
+          // input2.style.borderColor = '#352f44';
+          for(var i =0; i< buttons.length; i++) 
+            buttons[i].style.backgroundColor = '393e46';      
+        }
     });
   });
 
