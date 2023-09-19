8098477c0b2236520ae82ac3a9bd6517720cd351
arielcollatti <gabriel.rodriguesm0rg@outlook.com>
2023-09-19 17:31:06 -0300

    Update (19/09) localStorage added

--- register.js
+++ register.js
@@ -50,54 +50,29 @@ const processForm = (e) => {
 
 document.addEventListener('DOMContentLoaded', initialize);
 
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    const registerForm = document.getElementById("registerForm");
-      
-    registerForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      window.location.href = "confirmation.html"; 
-    });
-  });
-*/
-
 document.addEventListener("DOMContentLoaded", function () {
   const modeToggle = document.querySelector('.modeToggle');
   const body = document.body;
   const container = document.getElementById('container');
-  // const input = document.getElementById('password');
-  // const input2 = document.getElementById('name');
-  const button = document.getElementById('button');
-
-  const inputs = document.getElementsByTagName("input");
   const buttons = document.getElementsByTagName('button');
 
-  modeToggle.addEventListener('click', function () {
-
-    body.classList.toggle('darkMode');
-    container.classList.toggle('darkMode');
-
-    if (body.classList.contains('darkMode')) {
+  function setThemeFromLocalStorage() {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+      body.classList.add('darkMode');
+      container.classList.add('darkMode');
       container.style.backgroundColor = '#191A1C';
       container.style.background = '#191A1C';
       container.style.boxShadow = 'none';
-
-      //document.querySelectorAll("input")
-      //.forEach(input => input.style.borderColor = '#363434');
-      // for(var i =0; i< inputs.length; i++) 
-      //   inputs[i].style.borderColor = '#38598b';        
-      // input.style.borderColor = '#38598b';
-      // input2.style.borderColor = '#38598b';
       for (var i = 0; i < buttons.length; i++)
         buttons[i].style.backgroundColor = '#191A1C',
         buttons[i].style.color = 'white';
       document.querySelectorAll('a')
         .forEach(a => a.style.color = 'white');
       container.style.transition = '0.7s';
-    }
-
-    else {
+    } else {
+      body.classList.remove('darkMode');
+      container.classList.remove('darkMode');
       container.style.backgroundColor = '#FFFFFF';
       for (var i = 0; i < buttons.length; i++)
         buttons[i].style.backgroundColor = '#FFFFFF',
@@ -107,7 +82,18 @@ document.addEventListener("DOMContentLoaded", function () {
         .forEach(a => a.style.color = 'black');
         container.style.transition = '0.7s'
     }
-  });
-});
+  }
 
-// #606470(mais claro), #393e46(escuro), #222831(escurao)
+  function toggleTheme() {
+    if (body.classList.contains('darkMode')) {
+      localStorage.setItem('theme', 'light');
+    } else {
+      localStorage.setItem('theme', 'dark');
+    }
+    setThemeFromLocalStorage();
+  }
+
+  modeToggle.addEventListener('click', toggleTheme);
+
+  setThemeFromLocalStorage();
+});
