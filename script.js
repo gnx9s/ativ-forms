56035fd9f12667c4ba260b1efb91643b08d358a7
arielcollatti <gabriel.rodriguesm0rg@outlook.com>
2023-09-19 16:53:00 -0300

    Update (19/09) JS and CSS updated

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
-  document.addEventListener("DOMContentLoaded", function() {
-    const modeToggle = document.querySelector('.modeToggle');
-    const body = document.body;
-    const container = document.getElementById('container');
-    // const input = document.getElementById('password');
-    // const input2 = document.getElementById('name');
-    const button = document.getElementById('button');
+document.addEventListener("DOMContentLoaded", function () {
+  const modeToggle = document.querySelector('.modeToggle');
+  const body = document.body;
+  const container = document.getElementById('container');
+  // const input = document.getElementById('password');
+  // const input2 = document.getElementById('name');
+  const button = document.getElementById('button');
 
-    const inputs = document.getElementsByTagName('input');
-    const buttons = document.getElementsByTagName('button');
+  const inputs = document.getElementsByTagName('input');
+  const buttons = document.getElementsByTagName('button');
 
-    modeToggle.addEventListener('click', function() {
+  modeToggle.addEventListener('click', function () {
 
-      body.classList.toggle('darkMode');
-      container.classList.toggle('darkMode');
-      
-      if(body.classList.contains('darkMode'))
-      {
-        body.style.backgroundColor = '#161616';
-        body.style.transition = '0.7s';
-        container.style.border = 'solid 1px black';
-        container.style.boxShadow = '4px 4px 0 0 #060608';
-        container.style.transition = '0.7s';
-        container.style.backgroundColor = '#1F1F1F';
-        document.querySelectorAll("input")
-          .forEach(input => input.style.borderColor = '#363434');        
-        // for(var i =0; i< inputs.length; i++) 
-        //   inputs[i].style.borderColor = '#38598b';        
-        // input.style.borderColor = '#38598b';
-        // input2.style.borderColor = '#38598b';
-        for(var i =0; i< buttons.length; i++) 
-          buttons[i].style.backgroundColor = '#1F1F1F';
-      }
+    body.classList.toggle('darkMode');
+    container.classList.toggle('darkMode');
 
-      else 
-        {
-          body.style.backgroundColor = 'whitesmoke';
-          body.style.transition = '0.7s';
-          container.style.border = 'solid 1px #393E46';
-          container.style.boxShadow = '4px 4px 0 0 ';
-          container.style.backgroundColor = '393e46';
-          container.style.transition = '0.7s';
-          for(var i =0; i< inputs.length; i++) 
-            inputs[i].style.borderColor = '#352f44';        
-          // input.style.borderColor = '#352f44';
-          // input2.style.borderColor = '#352f44';
-          for(var i =0; i< buttons.length; i++) 
-            buttons[i].style.backgroundColor = '393e46';      
-        }
-    });
+    if (body.classList.contains('darkMode')) {
+      container.style.backgroundColor = '#191A1C';
+      container.style.background = '#191A1C';
+      container.style.boxShadow = 'none';
+
+      //document.querySelectorAll("input")
+      //.forEach(input => input.style.borderColor = '#363434');
+      // for(var i =0; i< inputs.length; i++) 
+      //   inputs[i].style.borderColor = '#38598b';        
+      // input.style.borderColor = '#38598b';
+      // input2.style.borderColor = '#38598b';
+      for (var i = 0; i < buttons.length; i++)
+        buttons[i].style.backgroundColor = '#191A1C',
+        buttons[i].style.color = 'white';
+      document.querySelectorAll('a')
+        .forEach(a => a.style.color = 'white');
+      container.style.transition = '0.7s';
+    }
+
+    else {
+      container.style.backgroundColor = '#FFFFFF';
+      for (var i = 0; i < buttons.length; i++)
+        buttons[i].style.backgroundColor = '#FFFFFF',
+        buttons[i].style.color = 'black';
+      container.style.boxShadow = '0 0 60px, 0 0 60px #ffffff'
+      document.querySelectorAll('a')
+        .forEach(a => a.style.color = 'black');
+        container.style.transition = '0.7s'
+    }
   });
+});
 
 
 // #606470(mais claro), #393e46(escuro), #222831(escurao)
